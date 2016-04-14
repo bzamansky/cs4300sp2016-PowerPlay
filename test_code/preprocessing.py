89b7e4e9b5a896cd36aebed670bc8fbb3e618677
Batya <batya.zamansky@gmail.com>
2016-04-14 18:11:01 -0400

    check what i did in index.html with the javascript, to get a feel for the variable stuff in django

--- test_code/preprocessing.py
+++ test_code/preprocessing.py
@@ -89,8 +89,10 @@ d_terms = d_vectorizer.get_feature_names()
 debate_term_matrix = d_vectorizer.transform(debates)
 
 
+
+
+
 print(d_terms.index('immigration'))
 print(d_terms[62800])
 print(debate_names)
 print(debate_term_matrix.toarray()[:,62800])
-print(debates_list[6]['file'])
