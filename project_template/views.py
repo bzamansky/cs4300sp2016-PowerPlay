2ec5494d5649ecc9ac7b4d13215eeefa111b63e0
Batya <batya.zamansky@gmail.com>
2016-04-19 14:21:07 -0400

    worked on getting histogram to page -- pair programming with Jackie

--- project_template/views.py
+++ project_template/views.py
@@ -7,6 +7,10 @@ from .form import QueryForm
 from .test import find_similar
 from .test import search_results
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 # Create your views here.
 def index(request):
@@ -25,6 +29,39 @@ def index(request):
 
         #http://stackoverflow.com/questions/30531990/matplotlib-into-a-django-template
 
+        candidates = total_mentions_candidate.keys()
+        cand_nums = []
+        for i,k in enumerate(total_mentions_candidate.keys()):
+            cand_nums.append(total_mentions_candidate[k])
+
+        N = len(candidates)
+        ind = np.arange(N)  # the x locations for the groups
+        width = 0.7       # the width of the bars
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        rects1 = ax.bar(ind, cand_nums, width, color='b')
+
+        # add some text for labels, title and axes ticks
+        ax.set_ylabel('Number of times said')
+        ax.set_title('Candidates')
+        ax.set_xticks(ind + width/2.)
+        ax.set_xticklabels(candidates)
+
+
+        def autolabel(rects):
+            # attach some text labels
+            for rect in rects:
+                height = rect.get_height()
+                ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                        '%d' % int(height),
+                        ha='center', va='bottom')
+
+        autolabel(rects1)
+        #plt.show()
+        plt.savefig('project_template/templates/project_template/candidates_plot')
+
+        plot = 'project_template/templates/project_template/candidates_plot.png'
         # paginator = Paginator(output_list, 10)
         # page = request.GET.get('page')
         # try:
@@ -35,5 +72,6 @@ def index(request):
         #     output = paginator.page(paginator.num_pages)
     return render_to_response('project_template/index.html', 
                           {'output': output,
+                          'plot':plot,
                            'magic_url': request.get_full_path()
                            })
