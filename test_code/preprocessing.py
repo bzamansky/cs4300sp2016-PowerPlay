430ec90891e48fe1e294944d81885b798b654ae0
Batya <batya.zamansky@gmail.com>
2016-05-02 14:08:05 -0400

    added stopwords

--- test_code/preprocessing.py
+++ test_code/preprocessing.py
@@ -111,6 +111,8 @@ names = [
 ]
 moderators = ["andrea","moderators", "hemmer", "gilmore", "maccallum", "dickerson", "cordes", "cooney", "obradovich", "cavuto", "bartiromo", "baker", "unknown", "ramos", "salinas ", "ramos ", "salinas", "tumulty", "question ", "question", "smith", "unknown [through translator", "unknown: ", "blitzer", "bash", "hewitt", "hannah debella, college student", "holt", "franchesca ramsey", "mitchell", "franta", "announcer", "brownlee", "tapper", " ", "tapper(?)", "unidentified male", "male", "regan", "cooper", "lemon", "kelly", "baier", "wallace", "", "...", "unidentified female", " kasich", "kasich: ", "moderator", "cuomo", "hannity", "unidentifiable", "woodruff", "ifill", "dinan", "quick", "harwood", "quintanilla", "cramer", "santelli", "epperson", "todd", "maddow", "strassel", "garrett", "audience", "wilkins", "lopez", "seib", "unidentified", "muir", "raddatz", "ham", "mcelveen", "o'connor", "josh jacob, college student", "louis", "audience member", "quick: ", "levesque", "???", "trump(?)", "cruz(?)", "arraras"]
 
+additional_stops = ['people','yes','no','think','know','just','want','lot','going','really','make','say','said','got','need','right','tell','like','ll','let','way','look','great','did']
+
 with open("all_debate_list.json", "r") as f:
     transcripts = json.load(f)
 
@@ -128,7 +130,7 @@ term_document_matrix = vectorizer.transform(speeches)
 #Modifying stopwords
 my_additional_stop_words = ['ve','don','t','um','uh']
 stop_words = text.ENGLISH_STOP_WORDS.union(my_additional_stop_words)
-stop_words = stop_words.union(moderators)
+stop_words = stop_words.union(moderators).union(additional_stops)
 
 #Sorting the term-doc stuff by candidate
 
